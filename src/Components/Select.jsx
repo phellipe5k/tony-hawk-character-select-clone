afeb9edda0e9934cd206eb7f6851c514e5e7d6f8
phellipe <phellipe501@gmail.com>
2020-09-20 01:28:48 -0300

    preventDefault()

--- src/Components/Select.jsx
+++ src/Components/Select.jsx
@@ -3,7 +3,7 @@ class Select extends Component {
 
   render() {
     return (
-      <a className='aimg-profile' href='#'>
+      <a onClick={(event) => {event.preventDefault()}}className='aimg-profile' href='#'>
         <img src={this.props.perfil} onClick={this.props.func} className={this.props.classes} alt={this.props.alt}/> 
         
       </a>
